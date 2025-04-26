05fa9f477559b04d8708ad34f013acb57d7e0236
Josh Simmons <josh.simmons@gmail.com>
2025-04-26 12:53:42 +0900

    Hero movement.

--- game.lua
+++ game.lua
@@ -1,43 +1,8 @@
-skel = {}
-
-function skel:new(o)
-  o = o or {}
-  setmetatable(o, self)
-  self.__index = self
-  return o
-end
-
-function skel:update()
-  self.anim:update()
-
-  self.pos.x += self.vel.x
-  if self.pos.x < 0 then
-    self.pos.x = -self.pos.x
-    self.vel.x = -self.vel.x
-  elseif self.pos.x + self.size >= self.bounds.w then
-    self.pos.x = self.bounds.w - (self.pos.x + self.size - self.bounds.w) - self.size
-    self.vel.x = -self.vel.x
-  end
-
-  self.pos.y += self.vel.y
-  if self.pos.y < 0 then
-    self.pos.y = -self.pos.y
-    self.vel.y = -self.vel.y
-  elseif self.pos.y + self.size >= self.bounds.h then
-    self.pos.y = self.bounds.h - (self.pos.y + self.size - self.bounds.h) - self.size
-    self.vel.y = -self.vel.y
-  end
-end
-
-function skel:draw()
-  self.anim:draw(self.pos.x, self.pos.y, self.vel.x < 0)
-end
-
 function _init()
   skels = {}
-  for i=1,10 do
+  for i=1,1 do
     add(skels,
-      skel:new{
+      hero:new{
         anim = anim:new{
           t = 0,
           frame = flr(rnd(4)) + 1,
@@ -47,8 +12,6 @@ function _init()
           h = 2,
         },
         pos = {x = flr(rnd(120)), y = flr(rnd(120))},
-        vel = {x = flr(rnd(6))-3, y = flr(rnd(6))-3},
-        size = 16,
         bounds = {w = 128, h = 128},
       })
   end
---
+++ hero.lua
@@ -0,0 +1,76 @@
+hero = {
+  WALKSPEED = 2,
+  SIZE = 8,
+}
+
+function hero:new(o)
+  o = o or {}
+  setmetatable(o, self)
+  self.__index = self
+  o.btn = btn()
+  o.lastbtn = o.btn
+  o.vel = {x = 0, y = 0}
+  o.facing = 1
+  return o
+end
+
+function hero:pressed(btni)
+  local btnmask = 1 << btni
+  return (not (self.lastbtn & btnmask == btnmask)) and (self.btn & btnmask == btnmask)
+end
+
+function hero:held(btni)
+  local btnmask = 1 << btni
+  return self.btn & btnmask == btnmask
+end
+
+function hero:update()
+  self.anim:update()
+
+  self.lastbtn = self.btn
+  self.btn = btn()
+
+  local lr = 0
+  if self:held(0) then
+    lr -= 1
+  end
+  if self:held(1) then
+    lr += 1
+  end
+  local ud = 0
+  if self:held(2) then
+    ud -= 1
+  elseif self:held(3) then
+    ud += 1
+  end
+
+  if lr != 0 then
+    self.facing = lr
+  end
+
+  local walkspeed = self.WALKSPEED
+  if lr != 0 and ud != 0 then
+    walkspeed /= sqrt(2)
+  end
+  self.vel.x = lr * walkspeed
+  self.vel.y = ud * walkspeed
+
+  self.pos.x += self.vel.x
+  if self.pos.x < 0 then
+    self.pos.x = 0
+  elseif self.pos.x + self.SIZE >= self.bounds.w then
+    self.pos.x = self.bounds.w - self.SIZE - 1
+  end
+
+  self.pos.y += self.vel.y
+  if self.pos.y < 0 then
+    self.pos.y = 0
+  elseif self.pos.y + self.SIZE >= self.bounds.h then
+    self.pos.y = self.bounds.h - self.SIZE - 1
+  end
+end
+
+function hero:draw()
+  self.anim:draw(self.pos.x, self.pos.y, self.facing < 0)
+end
+
