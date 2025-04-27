aa98744941b353a0dcf688e5740de5ca50c70855
SnoozeTime <benoit.eudier@hey.com>
2025-04-27 18:18:20 +0900

    rotation when dies

--- hero.lua
+++ hero.lua
@@ -220,7 +220,39 @@ function hero:draw()
     if self.blade then
       self.blade:draw(self.pos.x + (self.facing < 0 and -8 or 8), self.pos.y, self.facing < 0)
     end
-  elseif self.state == "caught_by_vine" or "drowning" then
+  elseif self.state == "caught_by_vine" then
     self.vine_catch_anim:draw(self.pos.x, self.pos.y, self.facing < 0)
+  elseif self.state == "drowning" then
+    -- dont ask me why.
+    spr_r(14, self.pos.x, self.pos.y, self.drown_counter*10, 1,1)
   end
 end
+
+
+
+
+function spr_r(s,x,y,a,w,h)
+  sw=(w or 1)*8
+  sh=(h or 1)*8
+  sx=(s%8)*8
+  sy=flr(s/8)*8
+  x0=flr(0.5*sw)
+  y0=flr(0.5*sh)
+  a=a/360
+  sa=sin(a)
+  ca=cos(a)
+  for ix=0,sw-1 do
+   for iy=0,sh-1 do
+    dx=ix-x0
+    dy=iy-y0
+    xx=flr(dx*ca-dy*sa+x0)
+    yy=flr(dx*sa+dy*ca+y0)
+    if (xx>=0 and xx<sw and yy>=0 and yy<=sh) then
+      if sget(sx+xx,sy+yy) != 6 then 
+        pset(x+ix,y+iy,sget(sx+xx,sy+yy))
+      end
+    end
+   end
+  end
+ end
+ 
