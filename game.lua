dc4ed9dd599f82aef1606d3004d652ff4d9d0ae7
Josh Simmons <josh.simmons@gmail.com>
2025-04-27 17:52:27 +0900

    Cleaning up globals from game

--- game.lua
+++ game.lua
@@ -3,39 +3,41 @@ function _init()
   menu_init()
 end
 
-
 function game_init(level_index)
-  level = level_index
-  LEVELS = {
-    {mushrooms=1, roses=1, start_pos={5,5}, vines=1, river={river_start={0,64},river_end={128,64}, bridge={64,64}}},
-    {mushrooms=3, roses=1, start_pos={5,5}, vines=2, river={river_start={0,64},river_end={128,64}, bridge={64,64}}},
+  local game = {
+    LEVELS = {
+      {mushrooms=1, roses=1, start_pos={5,5}, vines=1, river={river_start={0,64},river_end={128,64}, bridge={64,64}}},
+      {mushrooms=3, roses=1, start_pos={5,5}, vines=2, river={river_start={0,64},river_end={128,64}, bridge={64,64}}},
+    },
   }
-  local level_desc = LEVELS[level_index]
+
+  game.level = level_index
+  local level_desc = game.LEVELS[level_index]
 
   -- set the new callbacks
-  _update = game_update
-  _draw = game_draw
+  _update = function() game_update(game) end
+  _draw = function() game_draw(game) end
 
   music(0)
 
-  shroom_grid = grid:new{
+  game.shroom_grid = grid:new{
            -- 120 so we don't place shrooms just outside the bottom of the map
            bounds = {w = 120, h = 120},
            divisions = 10,
          }
 
   -- rivers etc
-  env = river:new{river_start=level_desc.river.river_start, river_end=level_desc.river.river_end, bridge=level_desc.river.bridge}
+  game.env = river:new{river_start=level_desc.river.river_start, river_end=level_desc.river.river_end, bridge=level_desc.river.bridge}
 
-  ninja = hero:new{
+  game.ninja = hero:new{
            pos = {x = level_desc.start_pos[1], y = level_desc.start_pos[2]},
            bounds = {w = 128, h = 128},
          }
   for i=1,level_desc.mushrooms do
     while true do  -- please don't take too long lmao
       local pos = {x = flr(rnd(120)), y = flr(rnd(120))}
-      if shroom_grid:empty(pos) then
-        shroom_grid:insert(pos, shroom:new{
+      if game.shroom_grid:empty(pos) then
+        game.shroom_grid:insert(pos, shroom:new{
           pos = pos,
         })
         break
@@ -43,102 +45,102 @@ function game_init(level_index)
     end
   end
 
-  spores = {}
+  game.spores = {}
 
-  roses = {}
+  game.roses = {}
   for i=1,level_desc.roses do
-    add(roses, rose:new{
+    add(game.roses, rose:new{
       pos = {x = flr(rnd(120-rose.SIZE)), y = flr(rnd(120-rose.SIZE))},
     })
   end
 
-  vines = {}
+  game.vines = {}
   for i=1,level_desc.vines do 
 
     local vine_x = flr(rnd(120))
     local vine_y = flr(rnd(120))
 
-    while env:in_river(vine_x, vine_y, vine.SIZE) do
+    while game.env:in_river(vine_x, vine_y, vine.SIZE) do
       vine_x = flr(rnd(120))
       vine_y = flr(rnd(120))
     end
-    add(vines, vine:new {
+    add(game.vines, vine:new {
       pos = {x = vine_x, y = vine_y}}
     )
   end
 end
 
-function game_draw()
+function game_draw(game)
   rectfill(0,0,127,127,0)
   map()
-  env:draw()
-  for i=1,#roses do
-    roses[i]:draw()
+  game.env:draw()
+  for i=1,#game.roses do
+    game.roses[i]:draw()
   end
-  for _,shroom in pairs(shroom_grid.grid) do
+  for _,shroom in pairs(game.shroom_grid.grid) do
     shroom:draw()
   end
-  for i=1,#spores do
-    spores[i]:draw()
+  for i=1,#game.spores do
+    game.spores[i]:draw()
   end
 
-  for i=1,#vines do
-    vines[i]:draw()
+  for i=1,#game.vines do
+    game.vines[i]:draw()
   end
-  ninja:draw()
+  game.ninja:draw()
 end
 
-function game_update()
-  env:update()
-  ninja:update(shroom_grid, vines)
+function game_update(game)
+  game.env:update()
+  game.ninja:update(game.shroom_grid, game.vines)
   local dead_vines = {}
-  for id,shroom in pairs(shroom_grid.grid) do
-    local message = shroom:update(roses)
+  for id,shroom in pairs(game.shroom_grid.grid) do
+    local message = shroom:update(game.roses)
     if message then
       if message.id == shroom.REMOVE then
-        shroom_grid.grid[id] = nil
+        game.shroom_grid.grid[id] = nil
       elseif message.id == shroom.NEW_SPORE then
-        if shroom_grid:empty(message.spore.target) then
-          add(spores, message.spore)
+        if game.shroom_grid:empty(message.spore.target) then
+          add(game.spores, message.spore)
         end
       end
     end
   end
   local dead_spores = {}
-  for i=1,#spores do
-    local new_shroom = spores[i]:update()
+  for i=1,#game.spores do
+    local new_shroom = game.spores[i]:update()
     if new_shroom then
-      if shroom_grid:empty(new_shroom.pos) then
-        for ri=1,#roses do
+      if game.shroom_grid:empty(new_shroom.pos) then
+        for ri=1,#game.roses do
           -- Kill any roses in this square.
-          if shroom_grid:key(new_shroom.pos) == shroom_grid:key(roses[ri]:center()) then
-            roses[ri]:wither()
+          if game.shroom_grid:key(new_shroom.pos) == game.shroom_grid:key(game.roses[ri]:center()) then
+            game.roses[ri]:wither()
           end
         end
-        shroom_grid:insert(new_shroom.pos, new_shroom)
+        game.shroom_grid:insert(new_shroom.pos, new_shroom)
       end
       add(dead_spores,i)
     end
   end
   for i=#dead_spores,1,-1 do
-    deli(spores,dead_spores[i])
+    deli(game.spores,dead_spores[i])
   end
   local dead_roses = {}
-  for i=1,#roses do
-    if not roses[i]:update() then
+  for i=1,#game.roses do
+    if not game.roses[i]:update() then
       add(dead_roses, i)
     end
   end
   for i=#dead_roses,1,-1 do
-    deli(roses,dead_roses[i])
+    deli(game.roses,dead_roses[i])
   end
 
-  if #roses <= 0 then
+  if #game.roses <= 0 then
     gameover_init()
   end
-  if next(shroom_grid.grid) == nil and #spores <= 0 and #vines <= 0 then
-    if level < #LEVELS then
-      level_switch_init(level + 1)
+  if next(game.shroom_grid.grid) == nil and #game.spores <= 0 and #game.vines <= 0 then
+    if game.level < #game.LEVELS then
+      level_switch_init(game.level + 1)
     else
       win_init()
     end
@@ -146,8 +148,8 @@ function game_update()
 
   -- Update the vines
   --------------------
-  for i=1,#vines do
-    local message = vines[i]:update(ninja, env)
+  for i=1,#game.vines do
+    local message = game.vines[i]:update(game.ninja, game.env)
     if message then
       if message.id == vine.REMOVE then
         add(dead_vines, i)
@@ -157,6 +159,6 @@ function game_update()
 
   -- clean up dead vines
   for i=#dead_vines,1,-1 do
-    deli(vines,dead_vines[i])
+    deli(game.vines,dead_vines[i])
   end
 end
